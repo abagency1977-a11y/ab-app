f1082776b1b677c5af5f34ef4bf25a85697dc3cd
harry harasudhan <harry01192@gmail.com>
2025-09-04 07:31:58 +0000

    Try fixing this error: `Runtime Error: Error: Cannot read properties of

--- src/hooks/use-mobile.tsx
+++ src/hooks/use-mobile.tsx
@@ -5,19 +5,23 @@ import * as React from "react"
 const MOBILE_BREAKPOINT = 768
 
 export function useIsMobile() {
-  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
+  const [isMobile, setIsMobile] = React.useState<boolean>(false)
 
   React.useEffect(() => {
-    if (typeof window === 'undefined') return;
-
     const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
     const onChange = () => {
       setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
     }
+    
+    // Set the initial value
+    onChange();
+    
     mql.addEventListener("change", onChange)
-    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    return () => mql.removeEventListener("change", onChange)
+
+    return () => {
+      mql.removeEventListener("change", onChange)
+    }
   }, [])
 
-  return !!isMobile
+  return isMobile
 }
