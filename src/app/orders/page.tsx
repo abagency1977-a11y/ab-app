0e774cf04c7e876415c2dec9f9a770e42ca626ed
harry harasudhan <harry01192@gmail.com>
2025-09-04 07:29:04 +0000

    fire

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -1,15 +1,13 @@
 
 import AppLayout from "@/components/app-layout";
-import { getCustomers, getProducts } from "@/lib/data";
+import { getCustomers, getProducts, getOrders } from "@/lib/data";
 import { OrdersClient } from "./orders-client";
-import { getDocs, collection } from "firebase/firestore";
-import { db } from "@/lib/firebase";
 import type { Order } from "@/lib/types";
 
 export default async function OrdersPage() {
     const customersPromise = getCustomers();
     const productsPromise = getProducts();
-    const ordersPromise = getDocs(collection(db, 'orders')).then(snapshot => snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() } as Order)));
+    const ordersPromise = getOrders();
 
     const [customers, products, orders] = await Promise.all([customersPromise, productsPromise, ordersPromise]);
 
